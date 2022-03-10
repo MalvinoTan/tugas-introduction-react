78f41a50c08b1aa284e5defd1d5e922f19a068ca
Malvino Austin Tanura <malvinoaustintanura@gmail.com>
2022-03-10 18:59:15 +0700

    Mengupload tugas Introduction React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './home/home';
+import ContactUs from './contact_us/contact_us';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+      <ContactUs />
     </div>
   );
 }
---
+++ src/home/home.js
@@ -0,0 +1,13 @@
+import './home.css'
+import icon from '../assets/img/home_icon.png'
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <img src={icon}></img>
+            <h2>INI BAGIAN HOME</h2>
+        </div>
+    );
+}
+
+export default Home;
